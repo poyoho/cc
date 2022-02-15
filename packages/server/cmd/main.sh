b23e318356eb0e7390bd69e5a6027cad4278e0d7
poyoho <907415276@qq.com>
2022-02-15 14:06:16 +0800

    chore: remove consul just use file to service discover

--- packages/server/cmd/main.sh
+++ packages/server/cmd/main.sh
@@ -1,13 +1,9 @@
 pushgateway_addr='10.12.198.50'
 pushgateway_port='9091'
 
-consul_addr='10.12.198.50'
-consul_port='8500'
-consul_server="$consul_addr:$consul_port"
-
 echo 🔴 register pushgateway service
-sh ./service-discover.init.sh http://$consul_server $pushgateway_addr 9091
-sh ./service-discover.init.sh http://$consul_server $pushgateway_addr 9092
+sh ./service-discover.init.sh $pushgateway_addr 9091
+sh ./service-discover.init.sh $pushgateway_addr 9092
 
 echo 🔴 init pushgateway group
 sh ./pushgateway.init.sh http://$pushgateway_addr:9091
--- packages/server/cmd/service-discover.init.sh
+++ packages/server/cmd/service-discover.init.sh
@@ -1,31 +1,8 @@
-# only can use the docker internal network
-# https://www.consul.io/docs/discovery/services
-
 source ./color.sh
 
-consul_addr=$1
 pushgateway_ip=$2
 pushgateway_port=$3
 
-function get_register_body {
-  register_body=$(cat << EOF
-{
-  "id": "pushgateway_$2",
-  "name": "pushgateway",
-  "tags": ["pushgateway"],
-  "address": "$1",
-  "port": $2,
-  "checks": [
-    {
-      "http": "http://$1:$2/-/healthy",
-      "interval": "5s"
-    }
-  ]
-}
-EOF
-)
-  echo $register_body
-}
+# TODO alert targets.json file
 
-curl -X PUT -d "$(get_register_body $pushgateway_ip $pushgateway_port)" $consul_addr/v1/agent/service/register
-white registe $(green pushgateway http://$pushgateway_ip:$pushgateway_port) to $(green consul $consul_addr/v1/agent/service/register)
+white registery $(green pushgateway http://$pushgateway_ip:$pushgateway_port)
